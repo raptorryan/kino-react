654239f5f5b11155f9e7b514ada34f19700df601
raptorryan <niemiryan@gmail.com>
2022-09-08 08:52:23 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Terminal.MixProject do
     [
       app: :terminal,
       version: "0.1.0",
-      elixir: "1.14.0-rc.0",
+      elixir: "1.14.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
